8e7f348b6b82a62d4948e826984b4cffe14389f5
Dina P <dinchik@bk.ru>
2023-10-29 01:52:18 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const stringArray = Math.abs(n).toString().split("");
+    const reverseStringArray = stringArray.reverse();
+    const resultString = reverseStringArray.join("");
+    const resultReverse = Number(resultString);
+    return resultReverse;
+};
